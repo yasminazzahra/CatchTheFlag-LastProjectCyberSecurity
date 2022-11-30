5c732950f959cecdc88196436b66cc364d280248
yasminazzahra <yasminazzahra.ya@gmail.com>
2022-11-30 16:52:48 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+(async () => {
+  await new Promise((e) => window.addEventListener("load", e)),
+    document.querySelector("form").addEventListener("submit", (e) => {
+      e.preventDefault();
+      const r = { u: "input[name=username]", p: "input[name=password]" },
+        t = {};
+      for (const e in r)
+        t[e] = btoa(document.querySelector(r[e]).value).replace(/=/g, "");
+      //username
+      return "Y3liZXI" !== t.u
+        ? alert("Incorrect Username") 
+        : //password
+        "c2VjdXJpdHk" !== t.p
+        ? alert("Incorrect Password")
+        : void window.location.assign("flagpage.html"); //alert("Correct Password");
+    });
+})();
